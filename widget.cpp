8d86d93a178b22618f94f69c7400fcbb32caf359
Utkarsh Tiwari <utiwari914@gmail.com>
2021-03-30 18:26:43 +0530

    update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -26,14 +26,6 @@ void Widget::on_messagePlainTextEdit_textChanged()
 
 void Widget::on_aboutButton_clicked()
 {
-    QString about = "";
-    about += "Project Submitted by: TEAM-29\n\n";
-    about += "1. Utkarsh Tiwari (1816015) - EIE\n";
-    about += "2. Baibhab Chakraborty (1816009) - EIE\n";
-    about += "3. Abhishek Baghele (1816010) - EIE\n";
-    about += "4. Siddharth Garlapati (1816060) - EIE\n";
-    about += "5. Mohit Kumar Kharwar (1816022) - EIE\n";
-    about += "6. Shrihari Khetre (1816039) - EIE\n";
-
+    QString about = "Made by - Utkarsh Tiwari";
     QMessageBox::about(this, "About this app", about);
 }
